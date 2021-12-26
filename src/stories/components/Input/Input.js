4d11b9c361d67f3285f5dc935b108e73b1a1c411
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2021-12-26 04:48:01 +0100

    Writing Stories and Addons

---
+++ src/stories/components/Input/Input.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Input.css'
+
+function Input(props) {
+  const { size='medium', ...rest } = props;
+
+  return (
+    <div>
+      <input className={`input ${size}`}  {...rest}/>
+    </div>
+  )
+}
+
+export default Input
